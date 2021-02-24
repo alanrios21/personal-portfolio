8a72ac5b5697cacf76a8bd871460a6f2da07d52d
Alan Rios <riosalan264@gmail.com>
2021-02-24 12:49:47 -0300

    Quite los carruseles 3d, mejore el cv y ajuste de nuevo el responsive

--- index.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu-toggle");
-const topNav = document.querySelector(".site-nav");
-menuBtn.addEventListener("click", function () {
-    topNav.classList.toggle("site-nav-open");
-    menuBtn.classList.toggle("menu-open");
-});
